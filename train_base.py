fd2eb5638b48aed77be0c7c8d449baca8fcd529d
Matthew Burke <mattburke122@gmail.com>
2020-02-21 11:42:07 -0600

    updtae train testing

--- train_base.py
+++ train_base.py
@@ -48,7 +48,7 @@ data_path = "./cityscape/prepped/"
 psp_gtfine.train(
     train_images=data_path + "images_prepped_train/",
     train_annotations=data_path + "annotations_prepped_train/",
-    checkpoints_path=data_path + "checkpoints/psp_gtfine", epochs=5
+    checkpoints_path="./checkpoints/psp_gtfine", epochs=5
 )
 
 # want to plot validation loss while training
@@ -60,10 +60,42 @@ psp_gtfine.train(
 # import matplotlib.pyplot as plt
 # plt.imshow(out)
 
+print("psp_gtfine")
 # evaluating the model
 print(psp_gtfine.evaluate_segmentation(inp_images_dir=data_path + "images_prepped_test/",
                                        annotations_dir=data_path + "annotations_prepped_test/"))
-
+print("pret_model")
 # evaluating the pretrained model
 print(pret_model.evaluate_segmentation(inp_images_dir=data_path + "images_prepped_test/",
                                        annotations_dir=data_path + "annotations_prepped_test/"))
+
+# Add discriminator onto model
+'''
+def discriminator():
+    return pret_model
+
+
+# define the combined generator and discriminator model, for updating the generator
+def make_gan(g_model, d_model):
+    # make weights in the discriminator not trainable
+    d_model.trainable = False
+    # get noise and label inputs from generator model
+    gen_noise, gen_label = g_model.input
+    # get image output from the generator model
+    gen_output = g_model.output
+
+    # Clamp generated values to > 0
+    gen_output = tf.clip_by_value(gen_output, 0, 1)
+
+    # Enforce symmetry for generated_maps
+    gen_output = tf.math.divide(tf.math.add(gen_output, tf.transpose(gen_output, perm=[0, 2, 1, 3])), 2)
+
+    # connect image output and label input from generator as inputs to discriminator
+    gan_output = d_model([gen_output, gen_label])
+    # define gan model as taking noise and label and outputting a classification
+    model = Model([gen_noise, gen_label], gan_output)
+    # compile model
+    opt = tf.keras.optimizers.Adam(learning_rate=1e-4, beta_1=0.5, beta_2=0.999)
+    model.compile(loss='binary_crossentropy', optimizer=opt)
+    return model
+'''
