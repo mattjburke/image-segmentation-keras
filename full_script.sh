bf16258e54356c247abe92c9db387fd9a1f0e752
mburke <mburke@phantoon.las.iastate.edu>
2020-03-03 22:45:44 -0600

    adding slurm files for record

---
+++ full_script.sh
@@ -0,0 +1,23 @@
+# module install pip3
+# echo "pip installed"
+# python3 setup.py install --user
+# echo "setup complete"
+# echo "cuda version is -------- before beginning setup.py install --user"
+# nvcc --version
+
+# python3 setup.py install --user
+
+# echo "cuda version is -------- after setup.py and before tensorflow upgrade"
+# nvcc --version
+
+# pip3 install --user --upgrade tensorflow-gpu --upgrade-strategy eager
+
+# echo "tensorflow version is"
+# tensorflow --version  # not a crrect call
+
+echo "cuda version is -------- after tensorflow upgrade in previous run"
+nvcc --version
+
+python3 train_base.py
+echo "trin_base complete"
+
