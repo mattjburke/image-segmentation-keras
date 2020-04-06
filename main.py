cf2c8416c4ce970087466b9b775d8c50ac2af50d
Matthew Burke <mattburke122@gmail.com>
2020-04-06 01:01:16 -0500

    training gen with larger input

--- main.py
+++ main.py
@@ -86,11 +86,11 @@ def train_alternately(gen_model=None, gen_iou_model=None, d_model=None, gan_mode
         # implement stopping condition
 
 # ------------------------- segnet -----------------------------------------
-gen_segnet = segnet(20, input_height=128, input_width=256, encoder_level=3)  # n_classes changed from 19 to 20
+gen_segnet = segnet(20, input_height=256, input_width=512, encoder_level=3)  # n_classes changed from 19 to 20
 gen_segnet.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', tf.keras.metrics.AUC()])
-# gen_checkpoints_path = get_path("gen_segnet")
-# train_gen(gen_segnet, gen_checkpoints_path, data_path=data_path)
-gen_segnet.load_weights("/work/LAS/jannesar-lab/mburke/image-segmentation-keras/checkpoints/gen_segnet-2020-03-30-12:21:46.457167/- 10- 0.44.hdf5")
+gen_checkpoints_path = get_path("gen_segnet")
+train_gen(gen_segnet, gen_checkpoints_path, data_path=data_path)
+# gen_segnet.load_weights("/work/LAS/jannesar-lab/mburke/image-segmentation-keras/checkpoints/gen_segnet-2020-03-30-12:21:46.457167/- 10- 0.44.hdf5")
 
 # Train my stacked input gan
 disc_segnet_stacked = gan_disc.discriminator(gen_segnet)
